2a8bab1c4dd756db07e988df7a25079bc4e6cf09
Kyria <kyria.brown@gmail.com>
2015-10-01 21:58:31 -0700

    add response check and game start

--- math_game.rb
+++ math_game.rb
@@ -1,5 +1,25 @@
-#@player1
-# @player2
+require 'pry'
+
+@player1 = {
+  name: '',
+  score: ''
+}
+@player2 = {
+  name: '',
+  score: ''
+}
+
+# def ready_players
+#   puts "Player 1, whats your name?"
+#   name = gets.chomp
+#   @player1[:name] = name
+#
+#   puts "Player 2, what's your name?"
+#   name = gets.chomp
+#   @player2[:name] = name
+#
+# end
+
 def generate_nums
   [Random.rand(1..20), Random.rand(1..20)]
 end
@@ -7,7 +27,27 @@ end
 def ask_question
   num1, num2 = generate_nums
   puts "Player what does #{num1} + #{num2} = ?"
-  answer = gets.chomp
+  @response = gets.chomp.to_i
+  @answer = num1 + num2
 end
 
-ask_question
+def check_response
+  @answer == @response ? (puts "Yes!") : (puts "Wrong!")
+end
+
+
+
+def play_game
+  puts "Player 1, whats your name?"
+  name = gets.chomp
+  @player1[:name] = name
+
+  puts "Player 2, what's your name?"
+  name = gets.chomp
+  @player2[:name] = name
+
+  generate_nums
+  ask_question
+  check_response
+end
+play_game
